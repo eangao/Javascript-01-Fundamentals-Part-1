8a16f94c5c3f25f6732f9ab5c25d2dae7d3a6c0d
elmar <elmarcera@gmail.com>
2023-10-18 16:52:58 +0800

    Data Types

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -9,6 +10,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Varialbe name convention
 let jonas_matilda = "JM";
 let $function = 27;
 
@@ -19,3 +21,24 @@ let myFirstJob = "Coder";
 let myCurrentJob = "Teacher";
 
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Elmar");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
