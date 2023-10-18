5a2cb097ddab5870906f396d091f903c2d3c178d
elmar <elmarcera@gmail.com>
2023-10-18 16:33:53 +0800

    Values and Variables

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Elmar");
+console.log(23);
+
+let firstName = "Elmar";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "Elmar";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
