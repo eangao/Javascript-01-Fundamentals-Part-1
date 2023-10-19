5991bc6e68ee3de29718b89df97ad3301da746dc
elmar <elmarcera@gmail.com>
2023-10-19 16:40:57 +0800

    The switch Statement

--- script.js
+++ script.js
@@ -443,15 +443,64 @@ m tall.
 // § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
 // § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
 
-const scoreDolphins = (96 + 108 + 89) / 3;
-const scoreKoalas = (88 + 91 + 110) / 3;
-console.log(scoreDolphins, scoreKoalas);
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
 
-if (scoreDolphins > scoreKoalas) {
-  console.log("Dolphins win the trophy 🏆");
-} else if (scoreKoalas > scoreDolphins) {
-  //same as -> scoreDolphins < scoreKoala
-  console.log("Koalas win the trophy 🏆");
-} else if (scoreDolphins === scoreKoalas) {
-  console.log("Both win the trophy 🏆");
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy 🏆");
+// } else if (scoreKoalas > scoreDolphins) {
+//   //same as -> scoreDolphins < scoreKoala
+//   console.log("Koalas win the trophy 🏆");
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log("Both win the trophy 🏆");
+// }
+
+////////////////////////////////////////
+// The switch Statement
+////////////////////////////////////////
+
+const day = "monday";
+
+// without the break,
+// the code simply continues executing
+
+// switch
+// switch (day) {
+//   case "monday": //  day === 'monday
+//     console.log("Plan course structure");
+//     console.log("Go to coding meetup");
+//     break;
+//   case "tuesday":
+//     console.log("Prepare theory videos");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Write code examples");
+//     break;
+//   case "friday":
+//     console.log("Record videos");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Enjoy the weekend :D");
+//     break;
+//   default:
+//     console.log("Not a valid day!");
+// }
+
+// if else
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid day!");
 }
